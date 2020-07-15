911fed38b22238d50e699bcadcbaf490f9080b19
Ilza <ilzammed2@gmail.com>
2020-07-15 17:24:07 -0300

    sort e map of the repositories

--- src/Components/Repositories/Repositories.js
+++ src/Components/Repositories/Repositories.js
@@ -1,20 +1,27 @@
 import React from 'react';
+import IconStar from '../../Images/star-icon.svg'
+
 import './Repositories.css';
 
-const Repositories = ({ repos_url }) => {
+const Repositories = ({ repos }) => {
     return (
 
-        <div>
-            {repos_url.map((repo) => {
-                console.log(repo)
-                return (
-                    <div>
-                        <h3>{repo.name}</h3>
-                        <p>{repo.description}</p>
-                        <p>estrela</p>
-                    </div>
-                )
-            })}
+        <div className="repos__container">
+            {repos
+                .sort((a, b) => b.stargazers_count - a.stargazers_count)
+                .map((repo, index) => {
+                    return (
+                        <div className="repoCard" key={`repo_${index + 1}`}>
+                            <h3 className="repoName">{repo.name}</h3>
+                            <p>{repo.description}</p>
+                            <div className="stars">
+                                <img src={IconStar} alt="icone estrela"/>
+                                <p>{repo.stargazers_count}</p>
+                            </div>
+                            <hr/>
+                        </div>
+                    )
+                })}
 
         </div>
     )
