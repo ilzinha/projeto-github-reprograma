f227de9d1b5afac8a48f6082af06896b40cd8850
Ilza <ilzammed2@gmail.com>
2020-07-15 17:24:57 -0300

    add icons

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,16 +1,36 @@
 import React from 'react'
+import IconLocation from '../../Images/location-icon.svg';
+
 import './Profile.css';
 
 const Profile = ({ name, photo, altPhoto, bio, location, repositories, followers, following }) => {
     return (
         <div>
-            <img src={photo} alt={altPhoto} />
-            <h1>{name}</h1>
-            <h4>{bio}</h4>
-            <p>{location}</p>
-            <p>{repositories}</p>
-            <p>{followers}</p>
-            <p>{following}</p>
+            <img className="photo" src={photo} alt={altPhoto} />
+
+            <div className="bio">
+                <h1>{name}</h1>
+                <h4>{bio}</h4>
+            </div>
+
+            <div className="iconNumbers">
+                <img src={IconLocation} alt="Icone de localização" />
+                <p>{location}</p>
+            </div>
+
+            <div className="iconNumbers">   
+                <p>Repositórios : <span>{repositories}</span></p>
+            </div>
+
+            <div className="iconNumbers">
+               
+                <p> Seguidores: <span>{followers}</span></p>
+            </div>
+
+            <div className="iconNumbers">
+                
+                <p>Seguindo: <span>{following}</span></p>
+            </div>
         </div>
     )
 }
